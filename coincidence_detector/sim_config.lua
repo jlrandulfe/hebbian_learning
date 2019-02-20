4adfe1a22de451ee7a2bc8edffda204b47af5d97
jlrandulfe <jlrandulfe@gmail.com>
2019-02-20 12:35:37 +0100

    Add basic config file for running several simulations

---
+++ coincidence_detector/sim_config.lua
@@ -0,0 +1,10 @@
+agents = {
+     "/home/jalop17/01-scripts/08-Lua/hebbian_learning/coincidence_detector/master_agent.lua"
+}
+param = {
+     {21},       -- Parameter '1', is a static parameter which is accessible by the agents.
+     {1,100},    -- Parameter '2', going from 1 to 100, with step increments of 1.
+     {1,0.1,10}, -- Parameter '3', going from 1 to 10, with step increments of 0.1.
+}
+sim = {
+}
