4f88674c14a7098c03371a6cc0bfe1ca188489ca
jlrandulfe <jlrandulfe@gmail.com>
2019-02-22 12:57:21 +0100

    Reorganize the main loop in different functions

--- growth_cone.lua
+++ growth_cone.lua
@@ -34,11 +34,11 @@ env_noise_mean = 10
 -- Agent properties
 move = false
 parent_soma_id = -1
-init = true
-initial_axon_link = true
+init = false
+initial_spine_link = true
 
 -- Neuron properties
-axon_link_length = 2
+spine_link_length = 2
 connected = false
 excited = false
 excitation_level = 0
@@ -63,7 +63,7 @@ function initializeAgent()
 	Speed = 100
 	GridMove = true
 
-    -- Coordinates tracker, for creating new axon agents
+    -- Coordinates tracker, for creating new spine agents
     coords = {x1=PositionX, y1=PositionY, x2=PositionX, y2=PositionY}
     -- Tables containing info about the coordinates and towards incomming
     -- electric pulses, and the raw data of such pulses.
@@ -74,75 +74,18 @@ end
 
 function takeStep()
 
-    if init == true then
-        Event.emit{speed=0, description="cone_init"}
-    end
-
-    -- Get the distance from the last location where an axon agent was created.
-    coords.x1 = PositionX
-    coords.y1 = PositionY
-    local distance = Math.calcDistance(coords)
-    
-    -- When the growth cone has travelled enough distance, a new axon segment
-    -- agent is created.
-    if distance > axon_link_length then
-        if not initial_axon_link then
-            new_agent = Agent.addAgent("spine.lua", coords.x2, coords.y2)
-        else
-            initial_axon_link = false
-        end
-        coords.x2 = PositionX
-        coords.y2 = PositionY
-	end
-
-    if excitation_level > 80 then
-        Moving = true
-    else
-        Moving = false
-    end
-
-    -- Decrease the excitation level with time
-    if excitation_level > 0 then
-        excitation_level = excitation_level - 1
-    elseif excitation_level < 0 then
-        excitation_level = excitation_level + 1
-    end
-
     absolute_time = absolute_time + STEP_RESOLUTION
 
-    -- Set the growth cone direction to the electric pulse source
-    local vx = 0
-    local vy = 0
-    if not connected then
-        for key, values in pairs(pulses_table) do
-            -- Calculate the absolute distance and its X, Y decomposition
-            dx = values[1] - PositionX
-            dy = values[2] - PositionY
-            distance = math.sqrt(math.pow(dx, 2)+math.pow(dy, 2))
-            if distance < 2 then
-                say("Connected\n")
-                connected = true
-            end
-            -- Calculate the unit vector pointing towards the source
-            orientationX = dx / distance
-            orientationY = dy / distance
-            -- Get the velocity vector
-            vx = vx + orientationX*values[3]
-            vy = vy + orientationY*values[3]
-        end
-        -- Environment noise.
-        local env_noise_angle = Stat.randomFloat(0, 2*math.pi)
-        local env_noise_amp = Stat.gaussianFloat(0, env_noise_mean)
-        vx = vx + (env_noise_amp * math.cos(env_noise_angle))
-        vy = vy + (env_noise_amp * math.sin(env_noise_angle))
-
+    if not init then
+        Event.emit{speed=0, description="cone_init"}
     else
-        vx = 0
-        vy = 0
+        -- Create a spine agent, if necessary
+        create_spine_agent()
+        -- Set the growth cone velocity based on the electric pulse sources
+        vx, vy = get_velocity()
+        Move.setVelocity{x=vx, y=vy}
     end
-    Move.setVelocity{x=vx, y=vy}
 
-    excitation_level = 100000
 end
 
 
@@ -176,7 +119,7 @@ function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)
         end
 
     elseif eventDescription == "assign_group" then
-        init = false
+        init = true
         parent_soma_id = sourceID
         movement = eventTable[1]
         -- If a 0 is received, the growth cone is static and does not grow
@@ -207,6 +150,60 @@ function handleEvent(sourceX, sourceY, sourceID, eventDescription, eventTable)
         end
     end
 
+end
+
+
+function create_spine_agent()
+    -- Evaluate distance to the last created spine agent, and create new one
+    -- if distance is big enough
+    coords.x1 = PositionX
+    coords.y1 = PositionY
+    local distance = Math.calcDistance(coords)
+    -- When the growth cone has travelled enough distance, a new spine segment
+    -- agent is created.
+    if distance > spine_link_length then
+        if not initial_spine_link then
+            new_agent = Agent.addAgent("spine.lua", coords.x2, coords.y2)
+        else
+            initial_spine_link = false
+        end
+        coords.x2 = PositionX
+        coords.y2 = PositionY
+	end
+end
+
+
+function get_velocity()
+    local vx = 0
+    local vy = 0
+    if not connected then
+        for key, values in pairs(pulses_table) do
+            -- Calculate the absolute distance and its X, Y decomposition
+            dx = values[1] - PositionX
+            dy = values[2] - PositionY
+            distance = math.sqrt(math.pow(dx, 2)+math.pow(dy, 2))
+            if distance < 2 then
+                say("Connected\n")
+                connected = true
+            end
+            -- Calculate the unit vector pointing towards the source
+            orientationX = dx / distance
+            orientationY = dy / distance
+            -- Get the velocity vector
+            vx = vx + orientationX*values[3]
+            vy = vy + orientationY*values[3]
+        end
+        -- Environment noise.
+        local env_noise_angle = Stat.randomFloat(0, 2*math.pi)
+        local env_noise_amp = Stat.gaussianFloat(0, env_noise_mean)
+        vx = vx + (env_noise_amp * math.cos(env_noise_angle))
+        vy = vy + (env_noise_amp * math.sin(env_noise_angle))
+
+    else
+        vx = 0
+        vy = 0
+    end
+    return vx, vy
 end
 
 
